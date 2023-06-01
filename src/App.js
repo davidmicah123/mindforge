899e800e0472dbb0da7b745a5bdb21846f73ee13
davidmicah123 <micahdanielsd@gmail.com>
2023-06-01 20:37:23 +0100

    Detaching footer component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,52 @@ import ceo from "./Assets/Images/user.avif";
 import Trustedby from './Components/Trustedby';
 
 function App() {
+    function Footer() {
+        return <footer className={classes.footer}>
+            <div className={classes.footer_content_wrapper}>
+                <div className={classes.company_details_area}>
+                    <img src={logo} alt="" className={classes.logo} />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto laborum nobis natus.</p>
+                </div>
+
+                <div className={classes.footer_links_wrapper}>
+                    <ul className={classes.footer_links_list}>
+                        <li>About</li>
+                        <li>About Us</li>
+                        <li>Features</li>
+                        <li>News & Blog</li>
+                    </ul>
+                    <ul className={classes.footer_links_list}>
+                        <li>Company</li>
+                        <li>Why Video?</li>
+                        <li>Security</li>
+                        <li>Membership</li>
+                    </ul>
+                    <ul className={classes.footer_links_list}>
+                        <li>Support</li>
+                        <li>Faqs</li>
+                        <li>Support Center</li>
+                        <li>Contact Us</li>
+                    </ul>
+                    <ul className={classes.footer_links_list}>
+                        <li>Partner</li>
+                        <li>Our partner</li>
+                        <li>Become A Partner</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={classes.terms_and_condition_container}>
+                <div className={classes.tac_div_1}>
+                    <p>@Video 2023. All rights reserved</p>
+                </div>
+                <div className={classes.tac_div_2}>
+                    <p>Terms of agreement</p>
+                    <p>oprivacy policy</p>
+                </div>
+            </div>
+        </footer>;
+    }
+    
   return (
     <div>
       <nav className={classes.nabar}>
@@ -49,7 +95,9 @@ function App() {
               </div>
           </div>
       </section>
+
       <Trustedby />
+      
       <div className={classes.festures_section}>
           <div className={classes.features_section_content_wrapper}>
               <div className={classes.features_section_top}>
@@ -163,51 +211,11 @@ function App() {
           </div>
       </div>
 
-      <footer className={classes.footer}>
-          <div className={classes.footer_content_wrapper}>
-              <div className={classes.company_details_area}>
-                  <img src={logo} alt="" className={classes.logo} />
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto laborum nobis natus.</p>
-              </div>
-
-              <div className={classes.footer_links_wrapper}>
-                  <ul className={classes.footer_links_list}>
-                      <li>About</li>
-                      <li>About Us</li>
-                      <li>Features</li>
-                      <li>News & Blog</li>
-                  </ul>
-                  <ul className={classes.footer_links_list}>
-                      <li>Company</li>
-                      <li>Why Video?</li>
-                      <li>Security</li>
-                      <li>Membership</li>
-                  </ul>
-                  <ul className={classes.footer_links_list}>
-                      <li>Support</li>
-                      <li>Faqs</li>
-                      <li>Support Center</li>
-                      <li>Contact Us</li>
-                  </ul>
-                  <ul className={classes.footer_links_list}>
-                      <li>Partner</li>
-                      <li>Our partner</li>
-                      <li>Become A Partner</li>
-                  </ul>
-              </div>
-          </div>
-          <div className={classes.terms_and_condition_container}>
-              <div className={classes.tac_div_1}>
-                  <p>@Video 2023. All rights reserved</p>
-              </div>
-              <div className={classes.tac_div_2}>
-                  <p>Terms of agreement</p>
-                  <p>oprivacy policy</p>
-              </div>
-          </div>
-      </footer>    
+      {Footer()}    
     </div>
   );
+
+    
 }
 
 export default App;
